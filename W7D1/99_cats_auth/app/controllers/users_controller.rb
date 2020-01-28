81ea5bb801f240afb9662fa7a2533a92168195b0
Ori Molad <orimolad17@gmail.com>
2020-01-27 18:05:41 -0800

    work on authorization W7D

---
+++ W7D1/99_cats_auth/app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all 
+    render :index
+  end
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :password)
+  end
+
+end
